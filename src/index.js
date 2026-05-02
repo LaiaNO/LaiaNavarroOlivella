07c7c354e7154f66b9dcc84c0c51bb3371d8f0df
LaiaNO <41057949+LaiaNO@users.noreply.github.com>
2026-05-02 12:55:45 +0200

    Add row and footer
    Add eassy way to go up and a footer

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('visible');
+  } else {
+    backToTop.classList.remove('visible');
+  }
+});
